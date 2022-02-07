5314246b88d0413bbb679068f741131e65ea7831
Syed Abdullah <syedabdullahrahman@gmail.com>
2022-02-07 20:58:14 +0600

    Java Static Initializer Block - Use of static block & variables

--- src/main/java/com/abdullah/hackerrank/java/JavaStaticInitializerBlock.java
+++ src/main/java/com/abdullah/hackerrank/java/JavaStaticInitializerBlock.java
@@ -8,15 +8,25 @@ import java.util.Scanner;
  * @author Syed Abdullah
  */
 public class JavaStaticInitializerBlock {
-    public static void main(String[] args){
+    static int B,H;
+    static boolean flag;
+    static {
         Scanner sc = new Scanner(System.in);
-        int B = sc.nextInt();
-        int H = sc.nextInt();
+        B = sc.nextInt();
+        H = sc.nextInt();
         if(B<=0 || H<=0){
             System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
         } else{
+            flag = true;
+        }
+    }
+
+    public static void main(String[] args){
+        if(flag){
             int area=B*H;
             System.out.print(area);
         }
-    }
-}
+    }//end of main
+
+}//end of class
