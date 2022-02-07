7a34a797320d13bbc874921b6c64e525b4e4ad1f
Syed Abdullah <syedabdullahrahman@gmail.com>
2022-02-07 21:08:02 +0600

    String.valueOf(int) --- Done

---
+++ src/main/java/com/abdullah/hackerrank/java/JavaIntToString.java
@@ -0,0 +1,54 @@
+package com.abdullah.hackerrank.java;
+
+import java.util.*;
+import java.security.*;
+
+/**
+ * Created On:  8:51 PM 07-Feb-22
+ *
+ * @author Syed Abdullah
+ */
+
+public class JavaIntToString {
+    public static void main(String[] args) {
+
+        DoNotTerminate.forbidExit();
+
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in .nextInt();
+            in.close();
+            String s=  String.valueOf(n);//???; Complete this line below
+            //Write your code here
+
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
