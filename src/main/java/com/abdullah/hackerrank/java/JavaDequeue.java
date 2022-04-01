f37daf2a0f7fd34472e9f5d090aa148789b5847c
Syed Abdullah <syedabdullahrahman@gmail.com>
2022-04-01 23:50:27 +0600

    todo: java/JavaDequeue.java

---
+++ src/main/java/com/abdullah/hackerrank/java/JavaDequeue.java
@@ -0,0 +1,28 @@
+package com.abdullah.hackerrank.java;
+
+/**
+ * Created On:  1:12 AM 11-Feb-22
+ *
+ * @author Syed Abdullah
+ */
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+/**
+ * In computer science, a double-ended queue (dequeue, often abbreviated to deque, pronounced deck) is an abstract data type that generalizes a queue, for which elements can be added to or removed from either the front (head) or back (tail).
+ */
+public class JavaDequeue {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque deque = new ArrayDeque<>();
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+// todo
+        }
+    }
+}
