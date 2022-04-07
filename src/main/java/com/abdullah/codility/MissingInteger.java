10b87c2823a260a3da8302493326942952202038
abdullah <syedabdullahrahman@gmail.com>
2022-04-08 02:00:13 +0600

    MissingInteger - used set for solving

---
+++ src/main/java/com/abdullah/codility/MissingInteger.java
@@ -0,0 +1,33 @@
+package com.abdullah.codility;
+
+/**
+ * User: Syed Abdullah
+ * Date: 07-Apr-2022
+ * Time: 1:14 AM
+ * Link: https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/
+ */
+
+/*
+This is a demo task.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+ */
+public class MissingInteger {
+    // using Set<> to check and find the missing one
+
+}
